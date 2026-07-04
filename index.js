686425dcce11c1d2f8896790037eb72f8a3db480
Youngmin Yoon <yym980312@gmail.com>
2026-07-04 20:55:07 +0900

    fix(node.js): node js 테스트 시작

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// 순수 Node 문법 연습할 땐 외부 패키지 설치 자체가 필요 없어요. Node 자체에 내장된 모듈(http, fs, path 등)만 쓸 거니까요.
-
 const http = require('http');
-
-console.log('test')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
